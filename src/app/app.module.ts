1610fcc4d75f26fb8e1299e3d3b05e8d69908bd9
@stivenosuna9909 <javierstivenosuna@hotmail.com>
2021-03-18 16:14:13 -0500

    subiendo la 2da seccion del curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import{ HttpClientModule } from '@angular/common/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+//    AboutComponent,
+//    ContactComponent,
+//    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
